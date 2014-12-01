4dfee1756ce37cb35b735fa8e0144e5a14e15ff5
ESPINOZATEST-PU <ESPINOZATEST-PU@localhost.local>
2014-12-01 00:19:49 -0600

    ESPINOZATEST-PU pushing PullServerInfo.ps1

--- PullServerInfo.ps1
+++ PullServerInfo.ps1
@@ -1,6 +1,9 @@
 $pullServerInfo = @{
-"pullServerName" = ""
-"pullServerPrivateIp" = ""
+"pullServerName" = "ESPINOZATEST-PU"
+"pullServerPrivateIp" = "10.210.102.125"
 "pullServerPublicIp" = ""
-"region" = ""
+"region" = "ord"
+"isRackConnect" = $true
+"isManaged" = $false
+"defaultRegion" = "IAD"
 }
